675ef4742f557cc5b485ab95de6ea8194ce817df
Guilherme <guilherme@iMac-de-Guilherme.local>
2015-11-14 18:15:44 -0200

    admin login

--- functions.php
+++ functions.php
@@ -539,6 +539,42 @@ function getIdBySlug($_slug){
 
 
 
+// add a new logo to the login page
+function wptutsplus_login_logo() { 
+
+	$logo =  'wp-content/themes/isotopic/img/logo.svg';
+	
+   echo  '<style type="text/css">
+        .login #login h1 a {
+            background-image: url( '.$logo.' );
+        }
+
+        .wp-core-ui .button-primary{
+        	border:none !important;
+        	background-color:#272F32;
+        }
+
+        .login form{
+        	box-shadow:none;
+        	-webkit-border-radius: 3px;
+			-moz-border-radius: 3px;
+			border-radius: 3px;
+        }
+        input[type="text"],input[type="password"] {
+        	border:none !important;
+        	box-shadow:none;
+        	background-color:#f0f0f0 !important;
+        	-webkit-border-radius: 3px;
+			-moz-border-radius: 3px;
+			border-radius: 3px;
+        }
+        input[type="checkbox"]{
+        	border-color:#fff !important;
+        	background-color:#f0f0f0;
+        }
+    </style>';
+}
+add_action( 'login_enqueue_scripts', 'wptutsplus_login_logo' );
 
 
 
