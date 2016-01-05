c900678993bf596142d80d39558078b3bc63d0d5
Guilherme <guilhermecruz@gmail.com>
2016-01-05 11:33:36 -0200

    update wp version

--- functions.php
+++ functions.php
@@ -726,7 +726,7 @@ add_action( 'login_enqueue_scripts', 'login_isotopic' );
 function remove_core_updates(){
     global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
-add_filter('pre_site_transient_update_core','remove_core_updates');
+//add_filter('pre_site_transient_update_core','remove_core_updates');
 add_filter('pre_site_transient_update_plugins','remove_core_updates');
 add_filter('pre_site_transient_update_themes','remove_core_updates');
 
